44d7558c1d05f3c251ae9b9ffe8b4660aed2a0c0
Juan Herrera <jupahefi@gmail.com>
2025-08-25 04:35:00 -0400

    Agregado cors

--- api.php
+++ api.php
@@ -6,11 +6,10 @@
  * Toda la lógica está en la base de datos
  */
 
-header('Content-Type: application/json; charset=UTF-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
+ header('Content-Type: application/json; charset=UTF-8');
+ header('Access-Control-Allow-Origin: https://clinica-beleza.equalitech.xyz');
+ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+ header('Access-Control-Allow-Headers: Content-Type, Authorization');
 // Manejar preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
